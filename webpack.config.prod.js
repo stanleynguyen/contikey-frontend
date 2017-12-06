5780e6c55da65db509f460606ec82e8f937b3850
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-12-06 19:14:24 +0800

    WIP add real app url

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -31,6 +31,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
+        BASE_URL: JSON.stringify('http://api.contikey.com'),
       },
     }),
   ],
