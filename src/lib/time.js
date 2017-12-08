a45e61f401088640c4854a01f59e4bb811be34df
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-12-08 10:58:45 +0800

    [Fix] Date format

--- src/lib/time.js
+++ src/lib/time.js
@@ -1,7 +1,38 @@
 import moment from 'moment';
 
-export function formatAgo(date) {
-  return moment.utc(date).fromNow();
+export function formatAgo(dateStr) {
+  const date = moment.utc(dateStr).toDate();
+  const seconds = Math.floor((new Date() - date) / 1000);
+  const days = Math.floor(seconds / 86400);
+
+  if (seconds < 10) return 'a few seconds ago';
+  if (days > 1) {
+    return `${date.toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    })} at ${date.toLocaleTimeString('en-GB', {
+      hour: 'numeric',
+      minute: 'numeric',
+    })}`;
+  } else if (days > 0) {
+    return `Yesterday at ${date.toLocaleTimeString('en-GB', {
+      hour: 'numeric',
+      minute: 'numeric',
+    })}`;
+  } else {
+    const hours = Math.floor(seconds / 3600);
+    if (hours > 0) {
+      return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    } else {
+      const minutes = Math.floor(seconds / 60);
+      if (minutes > 0) {
+        return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+      } else {
+        return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+      }
+    }
+  }
 }
 
 export const getMinutesRead = numWords =>
